73ec280eab1d13f8ec53264d5ecdeaaa4d5164d7
Manuel Vargas <manuelvargas1251@gmail.com>
2019-01-10 17:02:33 -0600

    UI

---
+++ index.js
@@ -0,0 +1,9 @@
+let express = require('express')
+let app = express()
+const path = require('path')
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.listen(3000)
