0316c50c28323703aee042b8a9fdc9a48c6cbc55
Manuel Vargas <manuelvargas1251@gmail.com>
2019-01-11 15:30:28 -0600

    Ready for crawler 🙌

--- index.js
+++ index.js
@@ -1,43 +1,72 @@
-let express = require('express')
-let app = express()
-let Crawler = require("crawler");
-const path = require('path')
-
 console.log("--starting server--")
+
+const express = require('express')
+const app = express()
+const Crawler = require("crawler");
+const path = require('path')
+const preURL = 'https://www.'
+const c = new Crawler({
+  maxConnections: 10,
+  // deafult callback function
+  callback: function (error, res, done) {
+    if (error) {
+      console.log(error);
+    } else {
+      var $ = res.$;
+      // $ is Cheerio by default
+      //a lean implementation of core jQuery designed specifically for the server
+      console.log($("title").text());
+    }
+    done();
+  }
+});
+
+
+// Homepage Router
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-// Handle the username being sent over
+// Username Router and Handler
 app.get('/search', function (req, res) {
-  //res.sendFile(path.join(__dirname + '/index.html'))
-  console.log(Object.keys(req.query)[0])
+
+  let username = Object.keys(req.query)[0]
+  if (username === undefined || username === '') {
+    console.error('no name')
+  }
 
   //crawl with the username!
+  // Queue just one URL, with default callback
+  //c.queue(preURL + 'amazon.com' + '')
+
+  c.queue([{
+    uri: preURL + 'google.com' + '/' + username,
+    // The global callback won't be called
+    callback: function (error, res, done) {
+      if (error) {
+        console.log(error);
+      } else {
+        //reponse from 
+        let $ = res.$;
+        // $ is Cheerio by default
+        //a lean implementation of core jQuery designed specifically for the server
+        console.log($("title").text());
+        console.log('Grabbed', res.body.length, 'bytes');
+      }
+      done();
+    }
+  }]);
+
+
+  // if URL resolves, send check
+  res.send("✔")
+
+  //else DNE or Private
+  //res.send("❌")
+
 })
 
 app.listen(3000)
 
 
-
-let c = new Crawler({
-    maxConnections : 10,
-    // This will be called for each crawled page
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            var $ = res.$;
-            // $ is Cheerio by default
-            //a lean implementation of core jQuery designed specifically for the server
-            console.log($("title").text());
-        }
-        done();
-    }
-});
 console.log("--ready to search--")
-
-// Queue just one URL, with default callback
-c.queue('https://www.amazon.com');
-
-
